5bae63c67debf509879e63c592ecc96daf65914c
akwapis1 <alec.w.kwapis@wmich.edu>
2019-10-06 19:54:32 -0400

    First Push

---
+++ EmailTest/src/main/java/com/amazonaws/samples/Main.java
@@ -0,0 +1,13 @@
+package com.amazonaws.samples;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Main");
+		
+		AmazonSESSample email = new AmazonSESSample();
+
+	}
+
+}
